022459d8d19b62c7f6715a9e748fc34b79d5a239
kdbanman <kdbanman@ualberta.ca>
2014-07-30 20:08:38 -0600

    these are the sparsest commits ever.  v3. graphy.

--- Cell.js
+++ Cell.js
@@ -1,12 +1,51 @@
-var Cell = function (initialState) {
-    this._state = initialState
-    this._nbrs = [];
+// basic semi-dynamic* graph library that maintains internal consistency with
+// node-level controls.
+//   * nodes do not change, but edges may be created/destroyed between them
+
+var Cell = function (initialState, index, parentGraph)
+{
+    this.state = initialState;
+    this.idx = index; // index (or key) within parent graph
+    this.graph = parentGraph; // array (or object) of other Cells
+    this.nbrs = {}; // hash set of parentGraph indices (or keys)
 }
 
-Cell.prototype.getState = function () {
-    //TODO
+// has no effect if neighbor link already exists.
+// neighbor Cells MUST already exist within the parent graph.
+Cell.prototype.addNbrs = function (coordArr)
+{
+    var that = this;
+    coordArr.forEach(function (coord) {
+        that.addNbr(coord);
+    });
 }
 
-Cell.prototype.setState = function (state) {
-    //TODO
+// has no effect if neighbor link already exists.
+// neighbor Cells MUST already exist within the parent graph.
+Cell.prototype.addNbr = function (coord)
+{
+    // add neighbor to list
+    if (!this.nbrs[coord]) this.nbrs[coord] = true;
+    // add self as neighbor to new neighbor
+    var nbr = this.graph[coord];
+    if (!nbr.nbrs[this.idx]) nbr.nbrs[this.idx] = true;
+}
+
+// has no effect if no neighbor link exists
+Cell.prototype.delNbrs = function (coordArr)
+{
+    var that = this;
+    coordArr.forEach(function (coord) {
+        that.delNbr(coord);
+    });
+}
+
+// has no effect if no neighbor link exists
+Cell.prototype.delNbr = function (coord)
+{
+    // remove neighbor from list
+    if (this.nbrs[coord]) delete this.nbrs[coord];
+    // remove self from neighbor list of ex-neigbor
+    var nbr = this.graph[coord];
+    if (nbr.nbrs[this.idx]) delete nbr.nbrs[this.idx];
 }
--- squarishRPS.js
+++ squarishRPS.js
@@ -1,145 +1,239 @@
-var w   = 200 // bl.ocks.org viewport width
-  , h   = 200 // bl.ocks.org viewport height
+var viewW = 800    // THIS MUST BE A SUPER NICE NUMBER
+    , viewH = 800  // THIS MUST BE A SUPER NICE NUMBER
 
-  , cw  = 5 // cellWidth
-  , ch  = 5 // cellHeight
+    , w = viewW / 2 
+    , h = viewW / 2
 
-  , ccx = w/cw // cell count x
-  , ccy = h/ch // cell count y
-  , delay = 10  // ms between generations
-  , xScale  = d3.scale.linear().domain([0, ccx]).rangeRound([0, ccx * cw])
-  , yScale  = d3.scale.linear().domain([0, ccy]).rangeRound([0, ccy * ch])
+    , cellSize = 10 // cellWidth
+    , padding = 10
 
-  , density = 1.0 // 1.0 for full moore neighborhoods
-  , depth = 1 // max range of moore neighborhood connections
+    , ccx = w/(cellSize + padding) // cell count x
+    , ccy = h/(cellSize + padding) // cell count y
 
-  , stateGraph = []
-  ;
+    , xScale = d3.scale.linear().domain([0, ccx])
+                                .range([0, ccx * (cellSize + padding)])
+    , yScale = d3.scale.linear().domain([0, ccy])
+                                .range([0, ccy * (cellSize + padding)])
+
+    , delay = 50 // ms between generations
+
+    , density = 0.3 // 1.0 for full moore neighborhoods
+    , depth = 2 // max range of moore neighborhood connections
+
+    , stateGraph = []
+    , nextStateGraph = []
+    , links = []
+
+    , iterating = false
+    ;
 
 function coord(x, y) {
-  return coord[x +','+ y] ||
-        (coord[x +','+ y] = ccx * ((ccy + y) % ccy) + ((ccx + x) % ccx));
+    return coord[x +','+ y] ||
+           (coord[x +','+ y] = ccx * ((ccy + y) % ccy) + ((ccx + x) % ccx));
 }
 
 // returns an array of coordinates (array indices) for stateGraph
-// TODO make this return an array of coordinates - the getter will be a Cell prop
-// TODO use depth and density controls
-function setMooreNbrs(graph, c, dist) {
-  dist = dist || 1;
+function mooreNbrs(c, dist, density) {
+    dist = dist || 1;
 
-  // attempt to get memoized coordinate array
-  if (setMooreNbrs[c + ',' + dist] === undefined) {
-    // generate memoized entry
-    setMooreNbrs[c + ',' + dist] = [];
-    var x = c % ccx;
-    var y = Math.floor(c / ccx);
 
-    // start from top left and push to nbr array clockwise
-    var nbrX = x - dist;
-    var nbrY = y - dist;
-    while (nbrX <= x + dist) {
-      setMooreNbrs[c + ',' + dist].push(coord(nbrX,nbrY));
-      nbrX++;
-    } // top row pushed
-    nbrX--;
-    nbrY++;
-    while (nbrY <= y + dist) {
-      setMooreNbrs[c + ',' + dist].push(coord(nbrX,nbrY));
-      nbrY++;
-    } // right col pushed
-    nbrY--;
-    nbrX--;
-    while (nbrX >= x - dist) {
-      setMooreNbrs[c + ',' + dist].push(coord(nbrX,nbrY));
-      nbrX--;
-    } // bottom row pushed
-    nbrX++;
-    nbrY--;
-    while (nbrY > y - dist) {
-      setMooreNbrs[c + ',' + dist].push(coord(nbrX,nbrY));
-      nbrY--;
+    // attempt to get memoized coordinate array
+    if (mooreNbrs[c + ',' + dist] === undefined) {
+
+        if (c % 1 !== 0) console.log('c must be integer');
+        if (dist % 1 !== 0) console.log('dist must be integer');
+
+        // generate memoized entry
+        mooreNbrs[c + ',' + dist] = [];
+
+        var pushNbr = function (pushX, pushY) {
+            if (pushX >= 0 && pushX < ccx &&
+                pushY >= 0 && pushY < ccy &&
+                Math.random() < density)
+                mooreNbrs[c + ',' + dist].push(coord(pushX,pushY));
+        };
+
+        var x = c % ccx;
+        var y = Math.floor(c / ccx);
+
+        // start from top left and push to nbr array clockwise
+        var nbrX = x - dist;
+        var nbrY = y - dist;
+        while (nbrX <= x + dist) {
+            pushNbr(nbrX, nbrY);
+            nbrX++;
+        } // top row pushed
+        nbrX--;
+        nbrY++;
+        while (nbrY <= y + dist) {
+            pushNbr(nbrX, nbrY);
+            nbrY++;
+        } // right col pushed
+        nbrY--;
+        nbrX--;
+        while (nbrX >= x - dist) {
+            pushNbr(nbrX, nbrY);
+            nbrX--;
+        } // bottom row pushed
+        nbrX++;
+        nbrY--;
+        while (nbrY > y - dist) {
+            pushNbr(nbrX, nbrY);
+            nbrY--;
+        }
     }
-  }
 
-  graph[c].__defineGetter__("nbrs", function () {
-    var coordArr = setMooreNbrs[c + ',' + dist];
-    var nbrArr = [];
-    coordArr.forEach( function (c) {
-      nbrArr.push(graph[c]);
-    });
-    return nbrArr;
-  });
+    return mooreNbrs[c + ',' + dist];
+}
+
+function mutatedNextStateGraph() {
+    var c,
+        coord,
+        nbr,
+        numRock,
+        numPaper,
+        numScissors;
+
+    for (c = 0; c < stateGraph.length; c++) {
+            numRock = 0;
+            numPaper = 0;
+            numScissors = 0;
+            for (coord in stateGraph[c].nbrs) {
+                if (stateGraph[coord].state === 0) numRock++;
+                else if (stateGraph[coord].state === 1) numPaper++;
+                else numScissors++;
+            }
+
+            if (stateGraph[c].state === 0 && numPaper > Math.sqrt(numRock))
+                nextStateGraph[c].state = 1;
+            else if (stateGraph[c].state === 1 && numScissors > Math.sqrt(numPaper))
+                nextStateGraph[c].state = 2;
+            else if (stateGraph[c].state === 2 && numRock > Math.sqrt(numScissors))
+                nextStateGraph[c].state = 0;
+            else 
+                nextStateGraph[c].state = stateGraph[c].state;
+
+        }
+    return nextStateGraph;
 }
 
 // assign initial cell states
 // 3 possible cell states - 0, 1, or 2
-// TODO probably have to initialize and maintain a links object for updateness
 d3.range(ccx * ccy).forEach(function(c) {
-  stateGraph[c] = {state: Math.floor(Math.random() * 3)};
-  stateGraph[c].nbrs = setMooreNbrs(stateGraph, c);
+    stateGraph[c] = new Cell(Math.floor(Math.random() * 3), c, stateGraph);
+    nextStateGraph[c] = new Cell(0, c, nextStateGraph);
 });
 
-var vis = d3.select('body').append('svg:svg')
-    .attr('width', w)
-    .attr('height', h);
+// set initial neighborhoods
+d3.range(ccx * ccy).forEach(function(c) {
+    var levelDensity = density; // density quadratically decreases with distance
+    for (var i = 1; i <= depth; i++) {
+        // generate neighbor coordinates
+        var nbrCoords = mooreNbrs(c, i, levelDensity);
 
-vis.selectAll('rect')
-    .data(stateGraph)
-  .enter().append('svg:rect')
-  .attr('width', cw)
-  .attr('height', ch)
-  .attr('x', function(d, i) { return xScale(i % ccx); })
-  .attr('y', function(d, i) { return yScale(i / ccx | 0); })
-  // class all cells as .cell
-  // map state 0 to rock, 1 to paper, 2 to scissors
-  .classed({'cell': true,
-            'rock': function(d) { return d.state === 0; },
-            'paper': function(d) { return d.state === 1; },
-            'scissors': function(d) { return d.state === 2; }});
+        // add coordinates to graph(s)
+        stateGraph[c].addNbrs(nbrCoords);
+        nextStateGraph[c].addNbrs(nbrCoords);
 
-function createNewGeneration() {
-  var c,
-      numRock,
-      numPaper,
-      numScissors,
-      nextStateGraph = [];
-  for (c = 0; c < stateGraph.length; c++) {
-      numRock = 0;
-      numPaper = 0;
-      numScissors = 0;
-      stateGraph[c].nbrs.forEach(function (cell) {
-        if (cell.state === 0) numRock++;
-        else if (cell.state === 1) numPaper++;
-        else numScissors++;
-      });
+        // put neighbors in links list for force layout
+        nbrCoords.forEach(function (coord) {
+            links.push({source: c, target: coord});
+        });
 
-      nextStateGraph.push({});
-      nextStateGraph[c].nbrs = setMooreNbrs(nextStateGraph, c);
-
-      if (stateGraph[c].state === 0 && numPaper > numRock)
-        nextStateGraph[c].state = 1;
-      else if (stateGraph[c].state === 1 && numScissors > numPaper)
-        nextStateGraph[c].state = 2;
-      else if (stateGraph[c].state === 2 && numRock > numScissors)
-        nextStateGraph[c].state = 0;
-      else 
-        nextStateGraph[c].state = stateGraph[c].state;
-
+        // quadratically decrease density
+        levelDensity *= levelDensity;
     }
-  return nextStateGraph;
+});
+
+var force = d3.layout.force()
+    .size([viewW, viewH])
+    .nodes(stateGraph)
+    .links(links)
+    .linkDistance(padding / 10)
+    .charge(-50)
+    .on('tick', tick)
+    .start();;
+
+var vis = d3.select('.output').append('svg')
+    .attr('width', viewW)
+    .attr('height', viewH);
+
+var link = vis.selectAll("line")
+    .data(force.links())
+    .enter().append("line");
+
+link.classed({'link': true,});
+
+var node = vis.selectAll('circle')
+        .data(force.nodes(), function (d) { return d.idx; })
+    .enter().append('circle')
+    .attr('r', cellSize)
+    .call(force.drag)
+    .on("click", function(d) {
+        if (d3.event.shiftKey) {
+            d.state = (d.state + 1) % 3;
+        }
+    });
+
+    // class all cells as .cell
+    // map state 0 to rock, 1 to paper, 2 to scissors
+node.classed({'cell': true,
+              'rock': function(d) { return d.state === 0; },
+              'paper': function(d) { return d.state === 1; },
+              'scissors': function(d) { return d.state === 2; }});
+
+function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
 }
 
-function animate() {
-  d3.selectAll('rect')
-    .data(stateGraph = createNewGeneration())
-  // class all cells as .cell
-  // map state 0 to rock, 1 to paper, 2 to scissors
-  .classed({'cell': true,
-            'rock': function(d) { return d.state === 0; },
-            'paper': function(d) { return d.state === 1; },
-            'scissors': function(d) { return d.state === 2; }});
+function iterate() {
+    if (iterating) {
+        var swap = mutatedNextStateGraph();
+        for (var i = 0; i < stateGraph.length; i++) {
+            stateGraph[i].state = swap[i].state;
 
-  return false;
+            var swapNbrs = stateGraph[i].nbrs;
+            stateGraph[i].nbrs = swap[i].nbrs;
+            swap[i].nbrs = swapNbrs;
+        }
+    }
+
+        node.data(stateGraph)
+        // class all cells as .cell
+        // map state 0 to rock, 1 to paper, 2 to scissors
+        .classed({'rock': function(d) { return d.state === 0; },
+                  'paper': function(d) { return d.state === 1; },
+                  'scissors': function(d) { return d.state === 2; }});
 }
 
-setInterval(animate, delay);
+setInterval(iterate, delay);
+
+document.onkeypress = function (e) {
+    e = e || window.event;
+    if (e.keyCode === 13) {
+        iterating = !iterating;
+        if (iterating) {
+            d3.select('body').style('background-color', '#223322');
+        } else {
+            d3.select('body').style('background-color', '#332222');
+        }
+    }
+}
+document.onkeydown = function (e) {
+    e = e || window.event;
+    if (e.shiftKey) {
+        //node.on('mousedown.drag', null);
+    }
+}
+document.onkeyup = function (e) {
+    e = e || window.event;
+    if (e.shiftKey) {
+        //node.call(force.drag);
+    }
+}
